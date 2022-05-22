383a0cc655e0e452f8a3e9b7fc736b3e026fb878
HaochenQ <haochenqi1997@gmail.com>
2022-05-22 19:53:55 +0800

    hide client id

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <Auth0Provider
     domain="cozy-home.au.auth0.com"
-    clientId="NR6cuqj4TxOBMtPV06Sr1TwcwrmAk733"
+    clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
